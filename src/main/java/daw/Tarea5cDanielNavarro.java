e83629d74db8cc4b22044d49081965446469cb84
Daniel <dnavmoy119@g.educaand.es>
2024-01-13 16:16:10 +0000

    primer commit

---
+++ src/main/java/daw/Tarea5cDanielNavarro.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package daw;
+
+/**
+ *
+ * @author Dan_n
+ */
+public class Tarea5cDanielNavarro {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
